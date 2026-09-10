e9a35bc4564b6f834f1d16f5e9c4f4abcef376a0
erivas <sugardressed@gmail.com>
2026-09-10 11:47:23 -0700

    Assignement Week 01

---
+++ scripts/week1.js
@@ -0,0 +1,4 @@
+document.getElementById('showName').addEventListener('click', function() {
+    document.getElementById("name").innerHTML = "<h2>Hello Class, I am Elizabeth! Welcome to Web Development !72.</h2>";
+    console.log("Console is working!");
+});
